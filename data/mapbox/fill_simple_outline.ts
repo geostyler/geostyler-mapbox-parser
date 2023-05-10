9f03fc2383a0625b19d8eb719c9babac043313e1
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-05-10 16:17:54 +0200

    feat(write): store metadata after splitting fill symbolizer
    Co-authored-by: Philippe Prevautel <philippe.prevautel@ign.fr>

--- data/mapbox/fill_simple_outline.ts
+++ data/mapbox/fill_simple_outline.ts
@@ -2,26 +2,28 @@ const fillSimpleFillOutline: any = {
   version: 8,
   name: 'Simple Fill With outline',
   layers: [{
-      id: 'Simple Fill With outline',
-      type: "fill",
-      paint: {
-        "fill-color": "#ff0000"
-      }
-    },
-    {
-      id: "Simple Fill With outline",
-      type: "line",
-      paint: {
-        "line-opacity": 0.5,
-        "line-color": "#00ff00",
-        "line-width": 2
-      },
-      layout: {
-        "line-cap": "butt",
-        "line-join": "round"
-      }
+    id: 'rule_0',
+    type: 'fill',
+    paint: {
+      'fill-color': '#ff0000'
     }
-  ]
+  },
+  {
+    id: 'rule_0',
+    type: 'line',
+    paint: {
+      'line-opacity': 0.5,
+      'line-color': '#00ff00',
+      'line-width': 2
+    },
+    layout: {
+      'line-cap': 'butt',
+      'line-join': 'round'
+    }
+  }],
+  metadata: {
+    splitIds: ['rule_0']
+  }
 };
 
 export default fillSimpleFillOutline;
--- data/styles/fill_simple_outline.ts
+++ data/styles/fill_simple_outline.ts
@@ -3,15 +3,15 @@ import { Style } from 'geostyler-style';
 const fillSimpleFillOutline: Style = {
   name: 'Simple Fill With outline',
   rules: [{
-    name: 'Simple Fill With outline',
+    name: 'rule_0',
     symbolizers: [{
-        kind: "Fill",
-        color: "#ff0000",
-        outlineColor: "#00ff00",
-        outlineWidth: 2,
-        outlineOpacity: 0.5,
-        outlineCap: "butt",
-        outlineJoin: "round"
+      kind: 'Fill',
+      color: '#ff0000',
+      outlineColor: '#00ff00',
+      outlineWidth: 2,
+      outlineOpacity: 0.5,
+      outlineCap: 'butt',
+      outlineJoin: 'round'
     }]
   }]
 };
