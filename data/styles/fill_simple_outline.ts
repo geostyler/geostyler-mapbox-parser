e1ee2e255dcee52b79399eb85c7d6e855ef5c829
pprevautel <philippe.prevautel@ign.fr>
2023-05-10 12:01:58 +0200

    Fix test and add new test from fill symbolizer with outline

---
+++ data/styles/fill_simple_outline.ts
@@ -0,0 +1,19 @@
+import { Style } from 'geostyler-style';
+
+const fillSimpleFillOutline: Style = {
+  name: 'Simple Fill With outline',
+  rules: [{
+    name: 'Simple Fill With outline',
+    symbolizers: [{
+        kind: "Fill",
+        color: "#ff0000",
+        outlineColor: "#00ff00",
+        outlineWidth: 2,
+        outlineOpacity: 0.5,
+        outlineCap: "butt",
+        outlineJoin: "round"
+    }]
+  }]
+};
+
+export default fillSimpleFillOutline;
